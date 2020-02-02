09222fbc3259d70d0d1e24078c0c965d330d3511
Benjamin Lacroix <blacroix@xebia.fr>
2020-02-02 21:06:00 +0100

    feat(auth): allow user to login

--- xqr_code/README.md
+++ xqr_code/README.md
@@ -1,16 +1,13 @@
-# xqr_code
+# XQR Code
 
-XQRCode
+## How to contribute
 
-## Getting Started
+You have to define environment variable in .env file (do not commit).
 
-This project is a starting point for a Flutter application.
+Required variables:
 
-A few resources to get you started if this is your first Flutter project:
+* AUTH_URL
+* CLIENT_ID
+* REALM
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+💡 You can find those variables on XQR Code web app.
--- xqr_code/lib/screen/organization_screen.dart
+++ xqr_code/lib/screen/organization_screen.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_auth0/flutter_auth0.dart';
-import 'package:xqr_code/constants.dart';
+
+import '../constants.dart';
 
 class OrganizationScreen extends StatefulWidget {
   OrganizationScreen({Key key}) : super(key: key);
@@ -12,60 +13,81 @@ class OrganizationScreen extends StatefulWidget {
 }
 
 class _OrganizationScreenState extends State<OrganizationScreen> {
+  Auth0 auth0;
+  Future<UserInfo> userInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    userInfo = _getUserInfo();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 45, 56, 75),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ClientInfoWidget()
-            ],
-          ),
+      backgroundColor: Color.fromARGB(255, 45, 56, 75),
+      body: Padding(
+        padding: EdgeInsets.all(48),
+        child: FutureBuilder<UserInfo>(
+          future: userInfo,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 64, bottom: 16),
+                    child: RichText(
+                      text: TextSpan(
+                          text: '',
+                          style: TextStyle(color: Colors.white),
+                          children: [
+                            TextSpan(
+                                text: 'Bonjour ${snapshot.data.nickname}',
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            TextSpan(
+                              text:
+                                  ', sélectionnez une organisation pour continuer.',
+                            ),
+                          ]),
+                    ),
+                  ),
+                  ListBody(
+                      children: snapshot.data.tenants
+                          .map((tenant) => RaisedButton(
+                                onPressed: () {},
+                                child: Text(tenant),
+                              ))
+                          .toList()),
+                ],
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+      ),
     );
+  }
+
+  Future<UserInfo> _getUserInfo() async {
+    auth0 = Auth0(
+        baseUrl: DotEnv().env[ENV_KEY_AUTH_URL],
+        clientId: DotEnv().env[ENV_KEY_CLIENT_ID]);
+    var token =
+        await FlutterSecureStorage().read(key: STORAGE_KEY_ACCESS_TOKEN);
+    var auth0Auth = Auth0Auth(auth0.auth.clientId, auth0.auth.client.baseUrl,
+        bearer: token);
+    var _info = await auth0Auth.getUserInfo();
+    return UserInfo(
+        _info['nickname'],
+        List<String>.from(
+            _info['$APP_NAMESPACE/claims/app_metadata']['tenants']));
   }
 }
 
-class ClientInfoWidget extends StatefulWidget {
-  ClientInfoWidget({Key key}) : super(key: key);
+class UserInfo {
+  final String nickname;
+  final List<String> tenants;
 
-  @override
-  ClientInfoWidgetState createState() => ClientInfoWidgetState();
-}
-
-class ClientInfoWidgetState extends State<ClientInfoWidget> {
-  Auth0 auth0;
-  String label = 'ok';
-
-  @override
-  void initState() {
-    auth0 = Auth0(baseUrl: DotEnv().env['AUTH_URL'], clientId: DotEnv().env['CLIENT_ID']);
-    var storage = FlutterSecureStorage();
-    var token = storage.read(key: Constants.of(context).accessTokenKey);
-    _printInfo(token);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      label,
-      style: TextStyle(color: Colors.white),
-    );
-  }
-
-  void _printInfo(Future<String> accessToken) async {
-    try {
-      var auth0Auth = Auth0Auth(auth0.auth.clientId, auth0.auth.client.baseUrl, bearer: await accessToken);
-      var info = await auth0Auth.getUserInfo();
-      String buffer = '';
-      info.forEach((k, v) => buffer = '$buffer\n$k: $v');
-      setState(() {
-        label = buffer;
-      });
-    } catch (e) {
-      print(e);
-    }
-  }
+  UserInfo(this.nickname, this.tenants);
 }
