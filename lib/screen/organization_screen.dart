dffc165891c3afb4bb3e81fd8975edb112bf3c2b
Benjamin Lacroix <blacroix@xebia.fr>
2020-02-04 22:35:43 +0100

    feat(events): choose event from list

--- lib/screen/organization_screen.dart
+++ lib/screen/organization_screen.dart
@@ -4,15 +4,16 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_auth0/flutter_auth0.dart';
 
 import '../constants.dart';
+import '../routes.dart';
 
-class OrganizationScreen extends StatefulWidget {
-  OrganizationScreen({Key key}) : super(key: key);
+class OrganizationsScreen extends StatefulWidget {
+  OrganizationsScreen({Key key}) : super(key: key);
 
   @override
-  _OrganizationScreenState createState() => _OrganizationScreenState();
+  _OrganizationsScreenState createState() => _OrganizationsScreenState();
 }
 
-class _OrganizationScreenState extends State<OrganizationScreen> {
+class _OrganizationsScreenState extends State<OrganizationsScreen> {
   Auth0 auth0;
   Future<UserInfo> userInfo;
 
@@ -23,56 +24,59 @@ class _OrganizationScreenState extends State<OrganizationScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 45, 56, 75),
-      body: Padding(
-        padding: EdgeInsets.all(48),
-        child: FutureBuilder<UserInfo>(
-          future: userInfo,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Column(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(top: 64, bottom: 16),
-                    child: RichText(
-                      text: TextSpan(
-                          text: '',
-                          style: TextStyle(color: Colors.white),
-                          children: [
-                            TextSpan(
-                                text: 'Bonjour ${snapshot.data.nickname}',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            TextSpan(
-                              text:
-                                  ', sélectionnez une organisation pour continuer.',
-                            ),
-                          ]),
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Color.fromARGB(255, 45, 56, 75),
+        body: Padding(
+          padding: EdgeInsets.all(48),
+          child: FutureBuilder<UserInfo>(
+            future: userInfo,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: 64, bottom: 16),
+                      child: RichText(
+                        text: TextSpan(
+                            text: '',
+                            style: TextStyle(color: Colors.white),
+                            children: [
+                              TextSpan(
+                                  text: 'Bonjour ${snapshot.data.nickname}',
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                              TextSpan(
+                                text:
+                                    ', sélectionnez une organisation pour continuer.',
+                              ),
+                            ]),
+                      ),
                     ),
-                  ),
-                  ListBody(
-                      children: snapshot.data.tenants
-                          .map((tenant) => RaisedButton(
-                                onPressed: () {},
-                                child: Text(tenant),
-                              ))
-                          .toList()),
-                ],
-              );
-            } else {
-              return Center(child: CircularProgressIndicator());
-            }
-          },
+                    ListBody(
+                        children: snapshot.data.tenants
+                            .map((tenant) => RaisedButton(
+                                  onPressed: () async {
+                                    await FlutterSecureStorage().write(
+                                        key: STORAGE_KEY_TENANT, value: tenant);
+                                    Navigator.pushNamed(context, Routes.events);
+                                  },
+                                  child: Text(tenant),
+                                ))
+                            .toList()),
+                  ],
+                );
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
         ),
-      ),
-    );
-  }
+      );
 
   Future<UserInfo> _getUserInfo() async {
     auth0 = Auth0(
-        baseUrl: DotEnv().env[ENV_KEY_AUTH_URL],
-        clientId: DotEnv().env[ENV_KEY_CLIENT_ID]);
+        baseUrl: DotEnv().env[ENV_KEY_OAUTH_AUTH_URL],
+        clientId: DotEnv().env[ENV_KEY_OAUTH_CLIENT_ID]);
     var token =
         await FlutterSecureStorage().read(key: STORAGE_KEY_ACCESS_TOKEN);
     var auth0Auth = Auth0Auth(auth0.auth.clientId, auth0.auth.client.baseUrl,
