e06eecb75c16f1098872b6ec81132cd1c263f4a4
Benjamin Lacroix <blacroix@xebia.fr>
2020-04-14 17:53:37 +0200

    refactor(architecture): use bloc pattern and add tests

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -41,4 +41,28 @@ class ApiService {
       throw Exception('Cannot get attendee');
     }
   }
+
+  Future<List<Attendee>> getVisitors() async {
+    final user =
+        User.fromJson(jsonDecode(await storage.read(key: STORAGE_KEY_USER)));
+    final accessToken = await storage.read(key: STORAGE_KEY_ACCESS_TOKEN);
+    final event =
+        Event.fromJson(jsonDecode(await storage.read(key: STORAGE_KEY_EVENT)));
+
+    final response = await http.get(
+        '${DotEnv().env[ENV_KEY_API_URL]}/${user.tenant}/events/${event.id}/visitors',
+        headers: {HttpHeaders.authorizationHeader: "Bearer $accessToken"});
+
+    if (response.statusCode == 200) {
+      final _rawVisitors = jsonDecode(response.body);
+      final _visitors = List<Attendee>();
+      for (var rawEvent in _rawVisitors) {
+        _visitors.add(Attendee.fromJson(rawEvent));
+      }
+      _visitors.sort((a, b) => a.firstName.compareTo(b.firstName));
+      return _visitors;
+    } else {
+      throw Exception('Failed to load visitors');
+    }
+  }
 }
---
+++ lib/visitors/visitors_bloc.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:x_qrcode/api/api_service.dart';
+import 'package:x_qrcode/common/bloc.dart';
+import 'package:x_qrcode/visitor/model/attendee_model.dart';
+
+class VisitorsBloc implements Bloc {
+  final ApiService apiService;
+
+  final _visitorsController = StreamController<List<Attendee>>();
+
+  List<Attendee> _visitors;
+
+  VisitorsBloc({@required this.apiService});
+
+  Stream<List<Attendee>> get visitorsStream => _visitorsController.stream;
+
+  Future loadVisitors() async {
+    _visitors = await apiService.getVisitors();
+    _visitorsController.sink.add(_visitors);
+    return _visitors; // for unit testing purpose
+  }
+
+  void searchVisitor(String query) {
+    _visitorsController.sink.add(_visitors
+        .where((v) => v.firstName.toLowerCase().contains(query))
+        .toList());
+  }
+
+  @override
+  void dispose() {
+    _visitorsController.close();
+  }
+}
